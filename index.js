e5325dac06af575f1e006950b2e1c5843b91f8a4
jaydendavis746-debug <jaydendavis746@gmail.com>
2026-08-18 11:57:00 +0100

    added environemnt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require('./app')
-const port = 3000
+require('dotenv').config()
+
+const port = process.env.PORT
 
 app.listen(port, ()=>{
     console.log(`app running on port ${port}`);
